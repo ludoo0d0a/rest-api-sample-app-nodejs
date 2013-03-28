d2a3d7e89f47ec6abd8145fc316d9e96e03d578b
aydiv <aydiv.c@gmail.com>
2013-03-28 15:27:01 +0530

    Adding basic sign-in / sign-up flow

--- app.js
+++ app.js
@@ -1,69 +1,53 @@
+var template_engine = 'dust', 
+	domain = 'localhost';
 
-/**
- * Module dependencies.
- */
-
-var template_engine = 'dust'
-	, domain = 'localhost';
-
-var express = require('express')
-  , routes = require('./routes')
-  , db = require('./lib/db')
-  , http = require('http')
-	, store = new express.session.MemoryStore
-  , path = require('path');
+var express = require('express'), 
+	routes = require('./routes'),	
+	http = require('http'),
+	store = new express.session.MemoryStore,
+	path = require('path'),
+	flash = require('connect-flash');
 
 var app = express();
 
 if ( template_engine == 'dust' ) {
-	var dust = require('dustjs-linkedin')
-	, cons = require('consolidate');
-
+	var dust = require('dustjs-linkedin'),
+		cons = require('consolidate');
 	app.engine('dust', cons.dust);
-
 } 
-app.configure(function(){
-
-  app.set('template_engine', template_engine);
-  app.set('domain', domain);
-  app.set('port', process.env.PORT || 8080);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', template_engine);
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(express.cookieParser('wigglybits'));
+app.configure(function() {
+	app.set('template_engine', template_engine);
+	app.set('domain', domain);
+	app.set('port', process.env.PORT || 8080);
+	app.set('views', __dirname + '/views');
+	app.set('view engine', template_engine);
+	app.use(express.favicon());
+	app.use(express.logger('dev'));	
+	app.use(express.bodyParser());	
+	app.use(express.cookieParser());
 	app.use(express.session({ secret: 'whatever', store: store }));
-  app.use(express.session());
-  app.use(app.router);
-  app.use(require('less-middleware')({ src: __dirname + '/public' }));
-  app.use(express.static(path.join(__dirname, 'public')));
-
-	//middleware
-	app.use(function(req, res, next){
-		if ( req.session.user ) {
-			req.session.logged_in = true;
-		}
-		res.locals.message = req.flash();
-		res.locals.session = req.session;
-		res.locals.q = req.body;
-		res.locals.err = false; 
-		next();
-	});
-
+	app.use(express.session());
+	app.use(flash());	
+	app.use(app.router);	
+	app.use(express.static(path.join(__dirname, 'public')));	
 });
 
 app.configure('development', function(){
-  app.use(express.errorHandler());
+	app.use(express.errorHandler());
+	app.locals.inspect = require('util').inspect;
 });
 
-app.locals.inspect = require('util').inspect;
 
 app.get('/', routes.index);
+
 app.get('/signup', routes.signup);
+app.post('/signup', routes.completesignup);
+
 app.get('/signin', routes.signin);
-app.get('/profile', routes.profile);
+app.post('/login', routes.dologin);
+
+app.get('/signout', routes.signout);
+app.get('/profile', routes.auth, routes.profile);
 
 var paypal_api = require('./lib/paypal-rest-api.js')();
 
--- routes/index.js
+++ routes/index.js
@@ -1,31 +1,74 @@
-
-/*
- * GET home page.
- */
+var db = require('../lib/db')();
+var paypal = require('../lib/paypal-rest-api')();
 
 exports.index = function(req, res){
 	res.locals.session = req.session;
-	res.render('index', {});
+	var message = (req.flash && req.flash('message')) ? req.flash('message') : [];	
+	res.render('index', {message: message});
 };
 
 
+exports.auth = function(req, res, next){
+	if(req.session.authenticated) {
+        next();
+    } else {
+        res.redirect('signin');
+    }
+};
+
 exports.signup = function(req, res){
-	res.locals.session = req.session;
+	res.locals.session = req.session;	
 	res.render('sign_up', {});
+};
+
+exports.completesignup = function(req, res){
+	res.locals.session = req.session;
+	
+	var user = req.body.user;
+	if(user.password != user.password_confirmation) {
+		res.render('sign_up', {message: [{desc: "Passwords do not match", type: "error"}]});
+	}
+	// save credit card
+	creditCardId = "";
+	db.createUser(user.email, user.password, creditCardId, function(err, response){
+		if(err) {
+			res.render('sign_up', {message: [{desc: err, type: "error"}]});
+		} else {
+			req.session.authenticated = true;
+			req.flash('message', [{desc: "You have been signed up successfully", type: "info"}]);			
+			res.redirect('');
+		}
+	});
 };
 
 
 exports.signin = function(req, res){
 	res.locals.session = req.session;
-	res.render('sign_in', {});
+	var message = (req.flash && req.flash('message')) ? req.flash('message') : [];	
+	res.render('sign_in', {message: message});
 };
 
-/*
+exports.dologin = function(req, res){
+	res.locals.session = req.session;
+	
+	var user = req.body.user;
+	db.authenticateUser(user.email, user.password, function(err, response){
+		if(err) {
+			req.flash('message', [{desc: err, type: "error"}]);			
+			res.redirect('signin');
+		} else {
+			req.session.authenticated = true;			
+			res.render('index', {});
+		}
+	});	
+};
+
 exports.signout = function(req, res){
 	res.locals.session = req.session;
-	res.render('sign_in', {});
+	req.session.authenticated = false;
+	res.redirect('/');
 };
-*/
+
 
 exports.profile = function(req, res){
 	res.locals.session = req.session;
