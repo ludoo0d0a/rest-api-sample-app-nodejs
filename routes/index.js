0260ba4befce0183b2a1b09469735867435419b1
aydiv <aydiv.c@gmail.com>
2013-03-28 16:49:12 +0530

    Adding CC save for signup and CC retrieval for profile pages

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,20 @@
 var db = require('../lib/db')();
 var paypal = require('../lib/paypal-rest-api')();
 
+
+var http_default_opts = {
+	'host': 'api.sandbox.paypal.com',
+	'port': '',
+	'headers': {
+		'Accept': 'application/json',
+		'Content-Type': 'application/json',
+		'Authorization': ''
+	}
+};
+var client_id = 'EBWKjlELKMYqRNQ6sYvFo64FtaRLRR5BdHEESmha49TM';
+var client_secret = 'EO422dn3gQLgDbuwqTjzrFgFtaRLRR5BdHEESmha49TM';
+paypal.configure(http_default_opts);
+
 exports.index = function(req, res){
 	res.locals.session = req.session;
 	var message = (req.flash && req.flash('message')) ? req.flash('message') : [];	
@@ -25,20 +39,38 @@ exports.completesignup = function(req, res){
 	res.locals.session = req.session;
 	
 	var user = req.body.user;
+	var userCard = user.credit_card;	
+	
 	if(user.password != user.password_confirmation) {
 		res.render('sign_up', {message: [{desc: "Passwords do not match", type: "error"}]});
-	}
-	// save credit card
-	creditCardId = "";
-	db.createUser(user.email, user.password, creditCardId, function(err, response){
-		if(err) {
-			res.render('sign_up', {message: [{desc: err, type: "error"}]});
-		} else {
-			req.session.authenticated = true;
-			req.flash('message', [{desc: "You have been signed up successfully", type: "info"}]);			
-			res.redirect('');
-		}
-	});
+	} else {
+		//TODO: Add card validation	
+		
+		paypal.generateToken(client_id, client_secret, function(generatedToken) {
+			token = generatedToken;	
+			http_default_opts.headers['Authorization'] = token;
+			card = {type: userCard.type, number: userCard.number, cvv2: userCard.cvv2, expire_month: userCard.expire_month, expire_year: userCard.expire_year };			
+			//TODO: Create user even when card details are not given
+			paypal.credit_card.create(card, http_default_opts, function(card, err) {
+				console.log(card);
+				cardId = (err) ? "" : card.id; 
+				db.createUser(user.email, user.password, card.id, function(dbErr, response) {
+					if(dbErr) {
+						res.render('sign_up', {message: [{desc: err, type: "error"}]});
+					} else {
+						if(err) {						
+							res.render('', {message: [{desc: "You have been signed up but we had trouble saving your card information.", type: "error"}]});
+						} else {
+							req.session.authenticated = true;
+							req.session.email = user.email;
+							req.flash('message', [{desc: "You have been signed up successfully", type: "info"}]);			
+							res.redirect('');						
+						}
+					}
+				});			
+			});	
+		});
+	}	
 };
 
 
@@ -57,7 +89,8 @@ exports.dologin = function(req, res){
 			req.flash('message', [{desc: err, type: "error"}]);			
 			res.redirect('signin');
 		} else {
-			req.session.authenticated = true;			
+			req.session.authenticated = true;
+			req.session.email = user.email;			
 			res.render('index', {});
 		}
 	});	
@@ -66,11 +99,31 @@ exports.dologin = function(req, res){
 exports.signout = function(req, res){
 	res.locals.session = req.session;
 	req.session.authenticated = false;
+	req.session.email = '';
 	res.redirect('/');
 };
 
 
 exports.profile = function(req, res){
 	res.locals.session = req.session;
-	res.render('profile', {});
+	db.getUser(req.session.email, function(err, user) {
+		if(err || !user) {			
+			console.log(err);
+			//TODO: Display error message to user
+			res.render('profile', { message: [{desc: "Could not retrieve profile information", type: "error"}]});
+		} else {		
+			paypal.generateToken(client_id, client_secret, function(generatedToken) {
+				token = generatedToken;	
+				http_default_opts.headers['Authorization'] = token;			
+				paypal.credit_card.get(user.card, http_default_opts, function(card, err) {
+					if(err) {						
+						res.render('profile', {user: user, message: [{desc: "Could not retrieve card information", type: "error"}]});
+					} else {
+						console.log("No err");
+						res.render('profile', {user: user, card: card});						
+					}
+				});	
+			});			
+		}
+	});	
 };
